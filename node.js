d4961b991c44712f0a6353d25e1c4daaf31e9e01
Ellozac <zlsy108@gmail.com>
2023-11-02 22:43:48 +1300

    requests/database working

--- node.js
+++ node.js
@@ -3,6 +3,7 @@ const sqlite = require("sqlite3").verbose();
 const app = express();
 const jwt = require("jsonwebtoken");
 const port = 8080;
+var userCheck = null;
 let db = new sqlite.Database('database.sqlite');
 db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, email TEXT);");
 app.use(express.static("public")); 
@@ -19,10 +20,27 @@ app.post("/loginReq", (req, res) => {
   res.sendStatus(200);
 });
 
+
 app.post("/signupInfo", (req, res) => {
   const { username, password, email } = req.body;
-  console.log(email, password, username)
-  res.sendStatus(200);
+  console.log(email, password, username);
+
+  if (!username || !password) {
+    return res.sendStatus(400); 
+  }
+
+  db.get("SELECT username FROM users WHERE username = ?", [username], (err, user) => {
+    if (user) {
+      return res.sendStatus(409);
+    }
+
+    db.run("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", [username, password, email], (err) => {
+      if (err) {
+        return res.sendStatus(500); 
+      }
+      return res.sendStatus(200); 
+    });
+  });
 });
 
 var server = app.listen(port, () => {
