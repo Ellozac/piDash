90cbb0c364b097cec5cf78c8fa87e13c09dde88b
Ellozac <zlsy108@gmail.com>
2023-11-02 00:20:13 +0000

    added databse and ready for branching.

--- node.js
+++ node.js
@@ -1,9 +1,10 @@
-const { MongoClient } = require('mongodb');
 const express = require("express");
+const sqlite = require("sqlite3").verbose();
 const app = express();
 const jwt = require("jsonwebtoken");
 const port = 8080;
-
+let db = new sqlite.Database('database.sqlite');
+db.run("CREATE TABLE IF NOT EXISTS users(id PRIMARY INTEGER AUTOINCREMENT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, email TEXT);");
 app.use(express.static("public")); 
 app.use(express.json());
 
@@ -14,8 +15,14 @@ app.get("/", (req, res) => {
 
 app.post("/loginReq", (req, res) => {
   const { username, password } = req.body;
-  console.log(username)
-})
+  // Add username password check then return signed JWT token 
+  res.sendStatus(200)
+});
+
+app.post("/signupInfo", (req, res) => {
+  // add signup handle
+  res.sendStatus(200)
+});
 
 var server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
