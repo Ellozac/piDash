1551b7e47bad78d1ae653c15fdd3811598f28a4c
Ellozac <zlsy108@gmail.com>
2023-11-03 03:55:34 +0000

    signup w/passhashing/storing

--- node.js
+++ node.js
@@ -2,13 +2,19 @@ const express = require("express");
 const sqlite = require("sqlite3").verbose();
 const app = express();
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const port = 8080;
 var userCheck = null;
 let db = new sqlite.Database('database.sqlite');
-db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, email TEXT);");
+db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL, email TEXT, salt BLOB NOT NULL);");
 app.use(express.static("public")); 
 app.use(express.json());
 
+function randIntBetween(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
 app.get("/", (req, res) => {
   res.redirect("/login.html");
 });
@@ -24,25 +30,33 @@ app.post("/loginReq", (req, res) => {
 app.post("/signupInfo", (req, res) => {
   const { username, password, email } = req.body;
   console.log(email, password, username);
-
   if (!username || !password) {
     return res.sendStatus(400); 
   }
 
   db.get("SELECT username FROM users WHERE username = ?", [username], (err, user) => {
+    if (err) {
+      console.log(err);
+    }
     if (user) {
       return res.sendStatus(409);
     }
-
-    db.run("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", [username, password, email], (err) => {
+    var salt = randIntBetween(10,30)
+    bcrypt.hash(password, salt, (err, hash) => {
+      var passwordHash = hash;
+      db.run("INSERT INTO users (username, password, email, salt) VALUES (?, ?, ?, ?)", [username, passwordHash, email, salt], (err) => {
       if (err) {
-        return res.sendStatus(500); 
+        return res.sendStatus(500);
+        console.log(err);
       }
       return res.sendStatus(200); 
+      });
+
     });
-  });
+
+     });
 });
 
-var server = app.listen(port, () => {
+var server = app.listen(8080,'0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
---
+++ public/js/signup.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+  document.getElementById('signup').addEventListener('submit', (event) => {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const signDetails = { username, password, email };
+    fetch("/signupInfo", {
+  method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(signDetails),
+    })
+    .then(response => response.json())
+    .then(status => {
+        console.log(status.status())
+      })
+  });
+})
+
